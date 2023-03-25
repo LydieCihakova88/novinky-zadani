35d95afb5426734a450325a32fb18b91a5dd4a70
Lýdie Čiháková <lydiecihakova@gmail.com>
2023-03-25 12:16:11 +0100

    Nový úkol

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const product1 = {
   image: 'img/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'img/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'img/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const bodyElm = document.querySelector('.body');
+bodyElm.background.color = '#e9e9e9';
